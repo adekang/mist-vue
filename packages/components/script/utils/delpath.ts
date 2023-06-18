53fee8ebfc341183d98680bbb1587e9705a7157b
adekang <adekang@163.com>
2023-06-18 16:20:26 +0800

    chore: 实现样式打包

---
+++ packages/components/script/utils/delpath.ts
@@ -0,0 +1,33 @@
+import fs from "fs";
+import { resolve } from "path";
+import { pkgPath } from "./paths";
+//保留的文件
+const stayFile = ["package.json", "README.md"];
+
+const delPath = async (path: string) => {
+  console.log("delPath", path);
+
+
+  let files: string[] = [];
+
+  if (fs.existsSync(path)) {
+    files = fs.readdirSync(path);
+
+    files.forEach(async (file) => {
+      let curPath = resolve(path, file);
+
+      if (fs.statSync(curPath).isDirectory()) {
+        // recurse
+        if (file != "node_modules") await delPath(curPath);
+      } else {
+        // delete file
+        if (!stayFile.includes(file)) {
+          fs.unlinkSync(curPath);
+        }
+      }
+    });
+
+    if (path !== `${pkgPath}/dist`) fs.rmdirSync(path);
+  }
+};
+export default delPath;
---
+++ packages/components/script/utils/paths.ts
@@ -0,0 +1,8 @@
+import { resolve } from "path";
+
+//组件库根目录 components
+export const componentPath = resolve(__dirname, "../../");
+
+//pkg根目录 packages
+export const pkgPath = resolve(__dirname, "../../../");
+
